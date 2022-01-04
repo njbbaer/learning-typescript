d5205d6bc825d4d35414064bd8dfbce5f6dbbf09
Nate Baer <njbbaer@gmail.com>
2022-01-03 22:43:42 -0800

    Finish basics section

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,1 +1,14 @@
-console.log('Time to get started...');
+function add(n1, n2, printResult, phrase) {
+    var result = n1 + n2;
+    if (printResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var phrase = "Result is: ";
+add(number1, number2, printResult, phrase);
